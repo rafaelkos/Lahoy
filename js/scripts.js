ce3d0938557ad767cadfb0eb3a93477a7e7a0a2a
Rafael Santos <rafaelkos@gmail.com>
2022-08-05 15:48:19 -0300

    Co-authored-by: Arthur Santos <aksantos@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,28 @@ var lahoyPuuid = "A46wqieoS2TwQVmDczVXS0JF3eAjO5yKI6x--ZfPemtOTkPf0Dgm5-w01IopoC
 var lahoySummonerId = "PqCUkydLgnv1hco2E0MxifjyC3fzcLFDBOF1P1aA3huSvGU6FYPJJPHUMg"
 var lahoyAccountId = "9PGOiIrjGayzgAoJKoZtiUJ1VifxAHE8wqPZwMq7GaOc3zhNNkQdfHtN"
 var lahoyProfileIconId = "903"
+
+
 function teste() {
     document.getElementById('teste1').innerHTML = 'Isto é um teste';
 }
 var url = 'https://br1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=' + riotToken
 $.getJSON(url, function (data) {
-        console.log(data);
-        $('#table1 > tbody').append('<tr><td>' + data.freeChampionIds + '</td></tr>')
-
+    $('#table1 > tbody').append('<tr><td>' + data.freeChampionIds + '</td></tr>')
+    
 });
+var summonerName = "Lahoy"
+var summonerInformationAPI = "https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + riotToken
+$.getJSON(summonerInformationAPI, function (data) {
+    
+    // console.log("summonerInformationHTML " + summonerInformationAPI);
+    // console.log("summonerInformationAPI " + data);
+})
+
+function onClickSummoner(summonerName){
+    summonerInformationAPI = "https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + riotToken
+    console.log("summonerInformationHTML " + summonerInformationAPI);
+    $.getJSON(summonerInformationAPI, function (data) {
+        // console.log("summonerInformationAPI " + data)
+    })
+}
