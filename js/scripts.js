918ef8bb71a4363621305cf2212a82cf73204693
Arthur Santos <kosarthur@gmail.com>
2022-08-05 18:18:11 +0100

    added token

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
+var riotToken = "RGAPI-323fb144-6cd3-4a21-973d-48a9280498bf"
+var puuidLahoy = ""
 function teste() {
     document.getElementById('teste1').innerHTML = 'Isto é um teste';
 }
-var url = 'https://br1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-323fb144-6cd3-4a21-973d-48a9280498bf'
+var url = 'https://br1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=' + riotToken
 $.getJSON(url, function (data) {
         console.log(data);
         $('#table1 > tbody').append('<tr><td>' + data.freeChampionIds + '</td></tr>')
